068104aa2dd594f182d9aee0c40b9d32c8ba79ac
nksfrank <niklas.frank@gmail.com>
2015-11-15 18:41:37 +0100

    Added mock data for parkingplaces

--- mockup.php
+++ mockup.php
@@ -1,23 +1,40 @@
 <?php
+if(isset($_GET['id'])) {
+    $spots = 8;
 
-$spots = 8;
+    $response = [];
 
-$response = [];
-
-for($i = 0; $i < $spots; $i++) {
-    $occupied = rand(0,99);
-    $data = null;
-    if($occupied < 60) {
-        $data["IsAvailable"] = true;
-        $data["Status"] = "LEDIG";
-        $data["CssClass"] = "green";
-    }else {
-        $data["IsAvailable"] = false;
-        $data["Status"] = "UPPTAGEN";
-        $data["CssClass"] = "red";
+    for($i = 0; $i < $spots; $i++) {
+        $occupied = rand(0,99);
+        $data = null;
+        if($occupied < 60) {
+            $data["IsAvailable"] = true;
+            $data["Status"] = "LEDIG";
+            $data["CssClass"] = "green";
+        }else {
+            $data["IsAvailable"] = false;
+            $data["Status"] = "UPPTAGEN";
+            $data["CssClass"] = "red";
+        }
+        $response[] = $data;
     }
-    $response[] = $data;
+
+    echo json_encode($response);
+}
+else {
+    $list = 5;
+
+    $response = [];
+
+    for($i = 0; $i < $list; $i++) {
+        $data['id'] = $i;
+        $data['name'] = "Parking Place " . $i;
+        $data['cssClass'] = "";
+        $data['spots'] = 10*($i+1);
+        $response[] = $data;
+    }
+
+    echo json_encode($response);
 }
 
-echo json_encode($response);
 ?>
