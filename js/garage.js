426b949f67a9378dfbc69285163d613878577c8b
nksfrank <niklas.frank@gmail.com>
2015-11-15 18:40:22 +0100

    moved responsibilities to new service

--- js/garage.js
+++ js/garage.js
@@ -1,43 +1,68 @@
-var garageApp = angular.module("garageApp", [])
+var garageApp = angular.module("garageApp", ['ngRoute'])
+.config(
+    function($routeProvider) {
+        $routeProvider.
+        when("/", {templateUrl:"partials/parkingplacelist.html", controller:"parkingController"}).
+        when("/ParkingPlace/", {templateUrl:"partials/parkingplacelist.html", controller:"parkingController"}).
+        when("/ParkingPlace/:id", {templateUrl:"partials/parkingplace.html", controller:"parkingplaceController"}).
+        otherwise({
+                    redirectTo: '/'
+                });
+    })
 
-.service("spots", function spots($http, $q, $rootScope) {
-    var spot = this;
-    spot.spotList = {};
+.service("spots", ['$http', '$q',
+    function spots($http, $q) {
+        var spot = this;
+        spot.spotList = {};
 
-    spot.GetSpots = function() {
-        var defer = $q.defer();
+        spot.GetAllParkingPlaces = function() {
+            var defer = $q.defer();
 
-        $http({
-            method: 'GET',
-            url: 'mockup.php',
-        })
-        .success(function(data){
-            spot.spotList = data;
-            defer.resolve(data);
-        })
-        .error(function(err) {
-            defer.reject(err);
-        });
+            $http({
+                method: 'GET',
+                url: 'mockup.php',
+            })
+            .success(function(data){
+                spot.spotList = data;
+                defer.resolve(data);
+            })
+            .error(function(err) {
+                defer.reject(err);
+            });
 
-        return defer.promise;
-    }
-
-    return spot;
-})
-
-.controller("AppController", ['$scope', '$interval', 'spots', '$timeout', '$q',
-    function($scope, $interval, spots, $timeout, $q) {
-        $scope.init = function() {
-
-            $scope.Spots = $scope.getSpots();
-            $interval(function() {
-                $scope.showMessage("Uppdaterar platser från servern", 2000);
-                $scope.getSpots()
-            }, 7000);
+            return defer.promise;
         }
 
-        $scope.getSpots = function() {
-            spots.GetSpots()
+        spot.GetParkingPlace = function(id) {
+            var defer = $q.defer();
+
+            $http({
+                method: 'GET',
+                url: 'mockup.php',
+                params: {id : id}
+            })
+            .success(function(data){
+                spot.spotList = data;
+                defer.resolve(data);
+            })
+            .error(function(err) {
+                defer.reject(err);
+            });
+
+            return defer.promise;
+        }
+
+        return spot;
+    }])
+
+.controller("parkingController", ['$scope', 'spots', '$timeout', '$q',
+    function($scope, spots, $timeout, $q) {
+        $scope.init = function() {
+            $scope.getAllParkingPlaces();
+        }
+
+        $scope.getAllParkingPlaces = function() {
+            spots.GetAllParkingPlaces()
             .then(
                 function(data) {
                     //success
@@ -48,6 +73,45 @@ var garageApp = angular.module("garageApp", [])
                     $scope.showMessage("Woops, någonting gick fel!", 2000);
                 });
         }
+
+        $scope.init();
+    }])
+
+.controller("parkingplaceController", ['$scope', '$interval', 'spots', '$timeout', '$q', '$routeParams',
+    function($scope, $interval, spots, $timeout, $q, $routeParams) {
+        $scope.init = function() {
+            $scope.getParkingPlace();
+
+            stop = $interval(function() {
+                $scope.getParkingPlace()
+            }, 7000);
+        }
+
+        $scope.getParkingPlace = function() {
+            spots.GetParkingPlace($routeParams.id)
+            .then(
+                function(data) {
+                    //success
+                    $scope.Spots = spots.spotList;
+                },
+                function(data) {
+                    //error
+                    $scope.showMessage("Woops, någonting gick fel!", 2000);
+                });
+        }
+
+        $scope.$on('$destroy', function () {
+            if (angular.isDefined(stop)) {
+                $interval.cancel(stop);
+                stop = undefined;
+            }
+        });
+
+        $scope.init();
+    }])
+
+.controller("AppController", ['$scope', '$interval', 'spots', '$timeout', '$q',
+    function($scope, $interval, spots, $timeout, $q) {
 
         $scope.showMessage = function(msg, time) {
             $scope.Messages = [{Message : msg},];
@@ -61,9 +125,4 @@ var garageApp = angular.module("garageApp", [])
         $scope.close = function(id) {
             $("#"+id).slideUp();
         };
-
-        $scope.Messages;
-        $scope.Spots = {};
-
-        $scope.init();
 }]);
